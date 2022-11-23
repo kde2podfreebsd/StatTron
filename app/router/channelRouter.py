dcc5de7409b17230b3fc80c096e4100528afa78e
Ivan Bogomolov <qhomop@gmail.com>
2022-11-23 15:31:29 +0300

    update channel router: add get's routes

--- app/router/channelRouter.py
+++ app/router/channelRouter.py
@@ -4,6 +4,7 @@ from flask import Blueprint
 from sqlalchemy import func
 import asyncio
 
+from utils.util import representation_channel
 
 from models.Accounts import Account
 from models.Channels import Channel
@@ -15,13 +16,21 @@ loop = asyncio.get_event_loop()
 @bp.route("/channel", methods=['GET'])
 def get_channels():
     if request.method == 'GET':
-        return Channel.query.filter(Channel.channel_id is not None).all()
+        channels = Channel.query.filter(Channel.channel_id is not None).all()
+        return jsonify(list(map(lambda x: representation_channel(x), channels)))
+
+@bp.route("/channel/<channel_id>", methods=['GET'])
+def get_channel(channel_id):
+    if request.method == 'GET':
+        channel = Channel.query.filter_by(channel_id=channel_id).first()
+        output = representation_channel(channel) if channel is not None else jsonify('Channel does not exist')
+        return jsonify(output)
 
 @bp.route("/channel/<username>", methods=['GET'])
 def get_account_channels(username):
     if request.method == 'GET':
         account = Account.query.filter_by(username=username).first()
-        return account.channels if account is not None else 'Account not found'
+        return jsonify(list(map(lambda x: representation_channel(x), account.channels)))
 
 #TODO: сделать методы join_channel и left_channel
 # @bp.route("/join_channel/<username>", methods=['POST'])
--- app/utils/util.py
+++ app/utils/util.py
@@ -8,3 +8,21 @@ def representation_account(account):
         "port": account.port,
         "public_key": account.public_key
     }
+
+
+def representation_channel(channel):
+    return {
+        "channel_id": channel.channel_id,
+        "is_scam": channel.is_scam,
+        "is_private": channel.is_private,
+        "title": channel.title,
+        "username": channel.username,
+        "members_count": channel.members_count,
+        "description": channel.description,
+        "category": channel.category,
+        "photo_big_file_id": channel.photo_big_file_id,
+        "photo_small_file_id": channel.photo_small_file_id,
+        "small_photo_path": channel.small_photo_path,
+        "average_views": channel.average_views,
+        "er_all": channel.er_all,
+    }
