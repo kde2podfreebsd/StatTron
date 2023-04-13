768a25c5da5f079d6a818bbb301e84827bb7061b
Ivan Bogomolov <qhomop@gmail.com>
2023-04-13 23:55:37 +0300

    Add channel catalog handlers & models: Master Node

--- MasterNode/main.py
+++ MasterNode/main.py
@@ -3,14 +3,20 @@ from fastapi import FastAPI
 from fastapi.routing import APIRouter
 
 from MasterNode.Handlers.MainPageHandlers import main_page_router
+from MasterNode.Handlers.СhannelСatalogHandlers import channel_catalog_router
 
-app = FastAPI(title="StatTron Main Node")
+app = FastAPI(title="StatTron Master Node")
 
 main_api_router = APIRouter()
 
 main_api_router.include_router(
     main_page_router, prefix="/main_page", tags=["main_page"]
 )
+
+main_api_router.include_router(
+    channel_catalog_router, prefix="/channel_catalog", tags=["channel_catalog"]
+)
+
 app.include_router(main_api_router)
 
 if __name__ == "__main__":
